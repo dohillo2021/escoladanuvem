636d3c2c81d9825d2a49f11d80a66ad4278f76dd
william <dohillo@gmail.com>
2024-11-13 20:58:58 -0300

    calculo_do_produto

---
+++ exercicios/calculo_do_produto.py
@@ -0,0 +1,8 @@
+#Calculo do produto
+
+numero1 = int(input("Coloque o primeiro número: "))
+numero2 = int(input("Coloque o segundo número: "))
+
+produto = numero1 * numero2
+
+pront(f"PROD * {produto}")
