89c93fff37d3402eb65e2bd7dff97d67c09dfb56
william <dohillo@gmail.com>
2024-11-14 20:30:19 -0300

    Exercicios aula 2

---
+++ exercicios/aula02/operadores_comparacao.py
@@ -0,0 +1,8 @@
+a,b = 10,3
+
+print(f"Igualdade: {a == b}")
+print(f"Diferente: {a != b}")
+print(f"Maior que: {a > b}")
+print(f"Menor que: {a < b}")
+print(f"Maior ou igual: {a >= b}")
+print(f"Maior ou igual: {a <= b}")
