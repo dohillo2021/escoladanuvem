9a040d1f34728a10202524d2a64a0d19b6671738
william <dohillo@gmail.com>
2024-12-03 22:04:25 -0300

    projeto brbit bitcoins

--- exercicios/aula 01/calculo_da_area_do_circulo.py
+++
@@ -1,9 +0,0 @@
-#Cálculo da Área do Círculo
-
-import math
-
-raio = float(input("Escreva o raio a ser calculado :"))
-
-area = math.pi * (raio ** 2)
-
-print(f"A área do círculo com o raio {raio} é {area:.4f}")
--- exercicios/aula 01/calculo_do_produto.py
+++
@@ -1,8 +0,0 @@
-#Cálculo do Produto
-
-numero1 = int(input("Coloque o primeiro número : "))
-numero2 = int(input("Coloque o segundo número : "))
-
-produto = numero1 * numero2
-
-print(f"PROD = {produto}")
